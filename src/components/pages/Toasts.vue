1c09d6ef1bacd292db95856eb38b3ab7cc991821
Devin Cook <devin.cook@CD.local>
2016-09-23 15:05:16 -0700

    adding untracked files

---
+++ src/components/pages/Toasts.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="row page-content">
+    <!--Title-->
+    <h1 class="col-xs-12 display-1">Toasts</h1>
+
+    <!--Card Content-->
+    <v-card title="Basic Toasts" class="col-xs-12">
+      <!--Button Markup-->
+      <div class="row headings">
+        <div class="col-xs-4"><strong>Markup</strong></div>
+        <div class="col-xs-8"><strong>Output</strong></div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-4">
+          <div>
+            <span class="selector">
+              {{ toastMarkup }}
+            </span>
+          </div>
+        </div>
+        <div class="col-xs-8">
+          <div>
+            <v-button @click="toggleToast('1')">toggle toast</v-button>
+          </div>
+        </div>
+      </div>
+
+      <!--Button Properties-->
+      <div class="row headings">
+        <div class="col-xs-4"><strong>Property</strong></div>
+        <div class="col-xs-8"><strong>Output</strong></div>
+      </div>
+
+      <!--Toast with Action-->
+      <div class="row centered">
+        <div class="col-xs-4">
+          <div class="no-margin">
+            <span class="selector">action="undo" :function="testAlert"</span>
+          </div>
+        </div>
+        <div class="col-xs-8">
+          <div>
+            <v-button @click="toggleToast('2')">Button</v-button>
+          </div>
+        </div>
+      </div>
+
+      <v-toast :toggle.sync="show1" text="Simple Toast"></v-toast>
+
+      <v-toast :toggle.sync="show2" text="Toast with Action" action="undo" :function="testAlert"></v-toast>
+
+    </v-card>
+  </div>
+</template>
+
+<script>
+  // Imports
+  import VCard from '../vex/Card.vue';
+  import VToast from '../vex/Toast.vue';
+  import VButton from '../vex/Button.vue';
+
+  // Exports
+  export default {
+
+    // Components
+    components: {
+      VCard,
+      VToast,
+      VButton
+    },
+
+    // Data
+    data () {
+      return {
+        show1: false,
+        show2: false,
+        toastMarkup: '<v-toast :show.sync="show" text="Simple Toast" action="undo"></v-toast>'
+      }
+    },
+
+    methods: {
+      toggleToast (number) {
+        if (+number === 1) {
+          this.show1 = true;
+        } else {
+          this.show2 = true;
+        }
+      },
+
+      testAlert () {
+        window.alert('dwadadad');
+      }
+    },
+
+
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+//Import or add any styles here
+</style>
---
+++ src/components/vex/Toast.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="v-toast-container" v-show="show" transition="toast">
+    <div class="v-toast">
+      <span>{{ text }}</span>
+      <v-button v-if="action" @click="function">{{ action }}</v-button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  @import '../../styles/components/_toasts';
+</style>
+<script>
+
+  import VButton from './Button.vue';
+  import ToastService from '../../services/ToastService';
+
+  export default {
+    components: {
+      VButton
+    },
+
+    props: [
+      'text',
+      'action',
+      'function',
+      'toggle',
+    ],
+
+    methods: {
+
+    },
+
+    data () {
+      return {
+        show: false
+      }
+    },
+
+    watch: {
+      'toggle': {
+        handler: function(val, oldVal) {
+          if (this.toggle) {
+            ToastService.queue(this);
+          }
+        },
+      }
+    }
+  }
+</script>
